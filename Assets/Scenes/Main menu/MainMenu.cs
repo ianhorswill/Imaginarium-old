6a52a2dc59024a02f7a0de57643de50a0d3bd18e
ianhorswill <ian@northwestern.edu>
2020-02-14 16:43:27 -0600

    Added debug log support to new UI

--- Assets/Scenes/Main menu/MainMenu.cs
+++ Assets/Scenes/Main menu/MainMenu.cs
@@ -2,6 +2,13 @@
 
 public class MainMenu : MonoBehaviour
 {
+    public TMPro.TextMeshProUGUI LogButtonText;
+
+    public void Start()
+    {
+        UpdateToggleButton();
+    }
+
     public void OnGUI()
     {
         Scenes.HandleSceneKeys();
@@ -10,5 +17,16 @@ public class MainMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void ToggleDebugLog()
+    {
+        LogFile.Enabled = !LogFile.Enabled;
+        UpdateToggleButton();
+    }
+
+    private void UpdateToggleButton()
+    {
+        LogButtonText.text = LogFile.Enabled ? "End debug log" : "Start debug log";
     }
 }
